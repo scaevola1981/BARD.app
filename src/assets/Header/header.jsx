0da0801feb3ee8dbd822d8a8078a9fa4025d97ba
dorobantuflorin <dorobantuflorin81@gmail.com>
2025-01-25 23:48:01 +0200

    Update  Categories and fixed button/images layout

--- src/assets/Header/header.jsx
+++ src/assets/Header/header.jsx
@@ -4,15 +4,21 @@ import { FaComment, FaHeart, FaBell, FaUser } from 'react-icons/fa';
 const Header = () => {
   return (
     <div className="header-container container-fluid">
+      
       <header className="header">
+        <div className='logo-container'>
+          <img className='logo-img' src="../public/foto-icons/logo.jpg" alt="Logo" />
+        </div>
         <div className="header-icons">
           <div className="chat-icon">
-            <FaComment />
+             <FaComment />
+             <span className='chat-span'>Chat</span>
           </div>
-          <FaHeart className="heart-icon" />
-          <FaBell className="notification-icon" />
+             <FaHeart className="heart-icon" />
+             <FaBell className="notification-icon" />
           <div className="my-account-icon">
-            <FaUser className="account-icon" />
+             <FaUser/>
+            <span className="account-span">Contul meu</span>
           </div>
         </div>
         <button className="header-btn"> Adauga anunt</button>
